4766236768450bb67d91e55ab547999797012a57
vlsk3104 <vlsk3104@gmail.com>
2021-02-13 23:53:57 +0900

    チャネルとRailsアプリを連携

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -1,5 +1,14 @@
 class LineBotController < ApplicationController
-  protect_from_forgery expect: [:callback]
+  protect_from_forgery except: [:callback]
   def callback
+  end
+
+  private
+
+  def client
+    @client ||= Line::Bot::Client.new{ |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
   end
 end
